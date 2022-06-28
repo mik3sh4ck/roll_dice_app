7e071b5ea55a67271459b3a252ebf8b450d66fd8
mik3sh4ck <michaelbusiness06@gmail.com>
2022-06-28 10:27:31 +0700

    adding button in dice

--- lib/main.dart
+++ lib/main.dart
@@ -16,14 +16,29 @@ void main() {
 }
 
 class DicePage extends StatelessWidget {
+  int randomNumber = 3;
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Image(
-          image: AssetImage('images/dice1.png'),
-        ),
-      ],
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print("$randomNumber");
+              },
+              child: Image.asset('images/dice$randomNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(onPressed: () {
+              print("right button got pressed");
+            },
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
